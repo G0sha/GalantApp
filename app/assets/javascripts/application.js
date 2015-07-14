425072f711e594e63a3d2fad7ab0d1589dd561e9
gem install rails <rails new>
2015-07-14 22:56:40 +0300

    point 5.2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,30 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+    function slider(x) {
+      var page = document.getElementById('slider-page');
+      for (var i = 1; i <5; i++) {
+        var menu = document.getElementById('slider-menu-'+i)
+        if (x == i) {
+          page.style.background='url(img/slider/slider-'+i+'.jpg) no-repeat left'
+          menu.style.background='url(img/slider/pointer.png) no-repeat left'
+          menu.style.color='#9a9898'
+        }else{
+          menu.style.background=''
+          menu.style.color='inherit'
+        }
+      }
+    };
+
+    function menu(x) {
+      for (var i = 1; i <9; i++) {
+        if (x == i) {
+          $('#menu-'+i).toggle(400);
+
+        }else{
+          $('#menu-'+i).hide(400);
+        }
+      }
+    };
