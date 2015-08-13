b2dd4a30337df959072cedcb0aaec21a1cd32850
Igor <gg0sha@ya.ru>
2015-08-13 15:28:14 +0300

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 root = "/opt/www/GalantApp/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/unicorn-err.log"
 stdout_path "#{root}/log/unicorn.log"
 
 listen "/tmp/unicorn.galantapp.sock"
